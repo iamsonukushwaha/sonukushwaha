e279f86a592abb82021e86e7f90ce0f9b90b3f23
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2026-06-08 21:52:23 +0530

    Create sonukushwaha.sh

---
+++ sonukushwaha.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Colors
+YELLOW='\033[33m'
+GREEN='\033[32m'
+CYAN='\033[36m'
+BLUE='\033[34m'
+MAGENTA='\033[35m'
+RESET='\033[0m'
+
+echo
+
+echo -e "✨ ${GREEN}Sonu Kumar Kushwaha${RESET} ✨"
+echo
+
+echo -e "I am a ${CYAN}Computer Science${RESET} student from ${YELLOW}Gopalganj, Bihar${RESET}, currently pursuing"
+echo -e "M.Tech in Computing Systems and Infrastructure at ${YELLOW}BITS Pilani${RESET}."
+echo
+
+echo -e "I ${YELLOW}work${RESET} as a Linux System Administrator at ${YELLOW}Wipro Technologies${RESET}."
+echo
+
+echo -e "Recognized with the Best ${YELLOW}Prodigy${RESET} Certificate (FY 2023–24),"
+echo -e "Top Contributor for ${YELLOW}Cost Savings${RESET} Award (FY 2024–25) and"
+echo -e "Received Unit Award for ${YELLOW}Red Hat${RESET} Q3 Challenge 2025."
+echo
+
+echo -e "Completed ${GREEN}BCA${RESET} from Lalit Narayan Mishra Institute, Patna,"
+echo -e "and ${GREEN}schooling${RESET} at Imperial Public School, Hathwa."
+echo
+
+echo -e "${BLUE}Website:${RESET}  https://singlebucks.blogspot.com"
+echo -e "${BLUE}GitHub:${RESET}   https://github.com/iamsonukushwaha"
+echo -e "${BLUE}LinkedIn:${RESET} https://www.linkedin.com/in/sonukumarkushwaha"
+echo -e "${BLUE}Twitter:${RESET}  https://twitter.com/iamsonukushwaha"
+echo
+
+echo -e "${MAGENTA}That’s me today – I'll be different tomorrow.${RESET}"
+echo
